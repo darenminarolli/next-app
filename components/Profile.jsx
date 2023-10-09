564da65b4655ec594fac4b19fee01dcfdce41236
darenminarolli <80909893+darenminarolli@users.noreply.github.com>
2023-10-09 11:40:57 +0200

    added some things

--- components/Profile.jsx
+++ components/Profile.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import PromptCard from "./PromptCard"
 
-const Profile = () => {
+const Profile = (props) => {
   return (
-    <div>Profile</div>
+    <>
+    <section className="w-full">
+        <h1 className="head_text text-left">
+        <span className="blue_gradient">{props.name}</span>
+         Profile</h1>
+         <p className="desc text-left">{props.desc}</p>
+         <div className='mt-10 prompt_layout'>
+      {props.data.map((post) => (
+        <PromptCard
+          key={post._id}
+          post={post}
+          handleEdit={()=> props.handleEdit && 
+          props.handleEdit(post)}
+          handleDelete={()=>props.handleDelete &&
+          props.handleDelete(post)}
+        />
+      ))}
+    </div>
+    </section>
+    </>
   )
 }
 
